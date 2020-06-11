ae9b8ae9ec6e49b543c24935b5fb6fb98e55cf49
Jeff Highsmith <junk@jeffhighsmith.com>
2020-06-11 17:20:16 -0400

    Arrays of animals and names, dangerous catches end the game.

--- main.ts
+++ main.ts
@@ -38,12 +38,27 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     music.baDing.play()
+    discovered_animal = otherSprite.vx / -10 - 1
+    sub.say("Yay,  " + animal_names[discovered_animal] + "!", 500)
     otherSprite.destroy()
-    info.changeScoreBy(1)
+    if (discovered_animal == 8) {
+        sub.say("Ow, stingers!")
+        game.over(false)
+    } else if (discovered_animal == 9) {
+        sub.say("Ack, teeth!")
+        game.over(false)
+    } else {
+        info.changeScoreBy(1)
+    }
 })
 let projectile: Sprite = null
+let animal_speed = 0
+let animal_choice = 0
+let discovered_animal = 0
+let animal_names: string[] = []
+let sub: Sprite = null
 scene.setBackgroundImage(img`
 a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a 
 a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a 
@@ -166,7 +181,7 @@ d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d d d d d d d 7 7 7 7 7 d d 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 
 `)
-let sub = sprites.create(img`
+sub = sprites.create(img`
 . . . . . . . . . . . f f f f f f . . . . . . . . . . . . . . . 
 . . . . . . . . . . f d e d e e e f . . . . . . . . . . . . . . 
 . . . . . . . . . . f d d e e e e f . . . . . . . . . . . . . . 
@@ -185,7 +200,92 @@ f f b b f d e d e d e d e d e d e d e d e d e d e d e f b b f f
 . . f f f f f f f f f f f f f f f f f f f f f f f f f f f f . . 
 `, SpriteKind.Player)
 controller.moveSprite(sub)
-let animal_sprites = [img`
+let animal_images = [img`
+. . . . . 7 7 7 . . . . . . . . 
+. . . . 7 7 7 . . . . . . . . . 
+. . . . 7 7 . . . . . 7 7 7 7 . 
+. . . . 7 8 . . . . . 7 7 7 7 7 
+. . . . 7 8 8 9 9 8 8 7 7 8 . . 
+. . . . 8 9 9 8 8 7 7 9 8 . . . 
+. f 7 7 8 9 9 8 8 7 7 9 9 . . . 
+7 7 7 7 7 8 7 7 9 9 8 8 7 7 . . 
+7 7 7 7 7 8 7 7 9 9 8 8 7 7 . . 
+. f 7 7 8 9 9 8 8 7 7 9 9 . . . 
+. . . . 8 9 9 8 8 7 7 9 8 . . . 
+. . . . 7 8 8 9 9 8 8 7 7 8 . . 
+. . . . 7 8 . . . . . 7 7 7 7 7 
+. . . . 7 7 . . . . . . 7 7 7 . 
+. . . . 7 7 7 . . . . . . . . . 
+. . . . . . 7 7 . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . 4 4 . . . . . . . . . . . 
+. . 4 4 . . . . . . . 4 4 . . . 
+. . 4 4 . . 4 . . . . . 4 4 . . 
+. . 4 4 4 4 4 . . 4 . . 4 4 . . 
+. 4 4 4 4 4 . . . 4 4 4 4 4 . . 
+. 4 4 . . . . . . . 4 4 4 4 4 . 
+. 4 . . . . . . . . . . . . 4 . 
+. 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . 
+. . 4 4 4 4 f 4 f 4 4 4 4 4 . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. 4 4 . 4 . . . . . . 4 . 4 4 . 
+. 4 . . 4 . . . . . . 4 . . 4 . 
+. . 4 . . 4 . . . . 4 . . 4 . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . 8 . . . . 
+. . . . . . . . . 9 8 8 . . . . 
+. . . . . . . 8 8 9 8 8 . . . 9 
+. . . . . 8 9 8 8 9 8 8 . . 9 9 
+. . . 8 8 8 9 8 8 9 8 8 . 9 9 9 
+. 8 8 f 8 f 9 8 8 9 8 8 9 9 9 9 
+9 8 8 8 8 8 9 8 8 9 8 8 9 9 9 9 
+. 8 8 8 8 8 9 8 8 9 8 8 9 9 9 9 
+. . . 8 8 8 9 8 8 9 8 8 . 9 9 9 
+. . . . . 8 9 8 8 9 8 8 . . 9 9 
+. . . . . . . 8 8 9 8 8 . . . 9 
+. . . . . . . . . 9 8 8 . . . . 
+. . . . . . . . . . . 8 . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . 5 4 4 4 4 5 . . . . . 
+. 4 . . 5 4 4 4 4 4 4 5 . . . . 
+. 4 . 5 4 4 f 4 f 4 4 4 5 . . 4 
+4 . . 5 4 4 4 4 4 4 4 4 5 . 4 . 
+4 . 4 5 4 4 4 4 4 4 4 4 5 . . 4 
+. 4 . 5 4 4 4 4 4 4 4 4 5 4 . 4 
+. . . 4 5 4 4 4 4 4 4 5 . 4 4 . 
+. . 4 4 . 5 4 4 4 4 5 4 4 . . . 
+. . 4 . . 4 5 5 5 5 4 . . 4 . . 
+. 4 . . 4 . . 4 . 4 . 4 . . 4 . 
+4 . . 4 . . 4 . . 4 . 4 . . 4 . 
+4 . 4 4 . 4 4 . 4 . . . 4 . . 4 
+4 . 4 . . 4 . . 4 . . . 4 . . 4 
+. . . 4 . . 4 . . 4 . . . 4 . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 2 2 . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . 1 1 2 2 . . . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+. . . 1 1 1 1 2 1 1 1 . . . 2 2 
+. . 1 1 1 1 2 1 1 1 2 1 . 2 2 2 
+. . 1 f 1 f 1 2 1 2 1 1 2 2 2 . 
+. 1 1 1 1 1 2 1 1 1 2 1 2 2 2 . 
+1 1 1 1 1 1 1 2 1 2 1 1 2 2 2 . 
+. 1 1 1 1 1 2 1 1 1 2 1 . 2 2 2 
+. . 1 1 1 1 1 2 1 1 1 . . . 2 2 
+. . . 1 1 1 1 1 1 2 . . . . . . 
+. . . . 1 1 1 2 2 2 2 . . . . . 
+. . . . . . . . . 2 2 2 . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
 . . . . . . 6 . . . . . . . . . 
 . . . . . . 6 . . . . . . . . . 
 . . . . . . 6 . . . . . . . . . 
@@ -202,6 +302,23 @@ let animal_sprites = [img`
 . . . . . 5 6 6 5 5 . 5 5 5 5 . 
 . . . . . 5 5 5 5 5 5 5 . . . . 
 . . . . . . . 5 5 5 5 . . . . . 
+`, img`
+b b b b b b b b b . . . . . . . 
+b b b f b c b b b . . . . . . . 
+b b b b b b c b b . . . . . . . 
+b f b b b b b c b . . . . . . . 
+b b b b b b b c b . . . . . . . 
+b c b b b b b b b b . . . . . . 
+b b c b b b b b b b b . . . . . 
+b b b c c b b b b . . . . . . . 
+b b b b b b b b b b . . . . . . 
+. . . . . b b . . b . . . . . . 
+. . . . . . b . . b b . . . . . 
+. . . . . . . . . . b b . . . . 
+. . . . . . . . . . . b b b b . 
+. . . . . . . . . . . . . . b b 
+. . . . . . . . . . . . . . . b 
+. . . . . . . . . . . . . . . . 
 `, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . 2 . 
@@ -222,71 +339,20 @@ let animal_sprites = [img`
 `, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . 8 . . . . 
-. . . . . . . . . 9 8 8 . . . . 
-. . . . . . . 8 8 9 8 8 . . . 9 
-. . . . . 8 9 8 8 9 8 8 . . 9 9 
-. . . 8 8 8 9 8 8 9 8 8 . 9 9 9 
-. 8 8 f 8 f 9 8 8 9 8 8 9 9 9 9 
-9 8 8 8 8 8 9 8 8 9 8 8 9 9 9 9 
-. 8 8 8 8 8 9 8 8 9 8 8 9 9 9 9 
-. . . 8 8 8 9 8 8 9 8 8 . 9 9 9 
-. . . . . 8 9 8 8 9 8 8 . . 9 9 
-. . . . . . . 8 8 9 8 8 . . . 9 
-. . . . . . . . . 9 8 8 . . . . 
-. . . . . . . . . . . 8 . . . . 
+. e . e . e . . e . . . . . . . 
+. e d d e d d e d . e . . . . . 
+. d d d d d d d d e d . e . . . 
+d d d d d d d d d d d e . . . d 
+d d d d d d d d d d d d . d d d 
+d f d f d d d d d d d d d d d . 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d . 
+d d d e e e e e e d d d . d d d 
+. d e e e e e e e e d . . . . d 
+. . e e e e e e e e . . . . . . 
+. . . d e e e d e d . . . . . . 
+. . d . . d . d . . d . . . . . 
 . . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . 4 4 . . . . . . . . . . . 
-. . 4 4 . . . . . . . 4 4 . . . 
-. . 4 4 . . 4 . . . . . 4 4 . . 
-. . 4 4 4 4 4 . . 4 . . 4 4 . . 
-. 4 4 4 4 4 . . . 4 4 4 4 4 . . 
-. 4 4 . . . . . . . 4 4 4 4 4 . 
-. 4 . . . . . . . . . . . . 4 . 
-. 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . 
-. . 4 4 4 4 f 4 f 4 4 4 4 4 . . 
-. . . 4 4 4 4 4 4 4 4 4 4 . . . 
-. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-. 4 4 . 4 . . . . . . 4 . 4 4 . 
-. 4 . . 4 . . . . . . 4 . . 4 . 
-. . 4 . . 4 . . . . 4 . . 4 . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . 2 2 2 2 . . . . . 
-. . . . . . 2 2 2 2 . . . . . . 
-. . . . . 1 1 2 2 . . . . . . . 
-. . . . 1 1 1 1 1 . . . . . . . 
-. . . 1 1 1 1 2 1 1 1 . . . 2 2 
-. . 1 1 1 1 2 1 1 1 2 1 . 2 2 2 
-. . 1 f 1 f 1 2 1 2 1 1 2 2 2 . 
-. 1 1 1 1 1 2 1 1 1 2 1 2 2 2 . 
-1 1 1 1 1 1 1 2 1 2 1 1 2 2 2 . 
-. 1 1 1 1 1 2 1 1 1 2 1 . 2 2 2 
-. . 1 1 1 1 1 2 1 1 1 . . . 2 2 
-. . . 1 1 1 1 1 1 2 . . . . . . 
-. . . . 1 1 1 2 2 2 2 . . . . . 
-. . . . . . . . . 2 2 2 . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . 7 7 7 . . . . . . . . 
-. . . . 7 7 7 . . . . . . . . . 
-. . . . 7 7 . . . . . 7 7 7 7 . 
-. . . . 7 8 . . . . . 7 7 7 7 7 
-. . . . 7 8 8 9 9 8 8 7 7 8 . . 
-. . . . 8 9 9 8 8 7 7 9 8 . . . 
-. f 7 7 8 9 9 8 8 7 7 9 9 . . . 
-7 7 7 7 7 8 7 7 9 9 8 8 7 7 . . 
-7 7 7 7 7 8 7 7 9 9 8 8 7 7 . . 
-. f 7 7 8 9 9 8 8 7 7 9 9 . . . 
-. . . . 8 9 9 8 8 7 7 9 8 . . . 
-. . . . 7 8 8 9 9 8 8 7 7 8 . . 
-. . . . 7 8 . . . . . 7 7 7 7 7 
-. . . . 7 7 . . . . . . 7 7 7 . 
-. . . . 7 7 7 . . . . . . . . . 
-. . . . . . 7 7 . . . . . . . . 
 `, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . d d d d . . . . . . 
@@ -304,59 +370,11 @@ d d f d f d d d d d d d d d d .
 . . . . . . . . . . . . . d d . 
 . . . . . . . . . . . . . d . . 
 . . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . 5 5 5 5 . . . . . . 
-. . . . . 5 4 4 4 4 5 . . . . . 
-. 4 . . 5 4 4 4 4 4 4 5 . . . . 
-. 4 . 5 4 4 f 4 f 4 4 4 5 . . 4 
-4 . . 5 4 4 4 4 4 4 4 4 5 . 4 . 
-4 . 4 5 4 4 4 4 4 4 4 4 5 . . 4 
-. 4 . 5 4 4 4 4 4 4 4 4 5 4 . 4 
-. . . 4 5 4 4 4 4 4 4 5 . 4 4 . 
-. . 4 4 . 5 4 4 4 4 5 4 4 . . . 
-. . 4 . . 4 5 5 5 5 4 . . 4 . . 
-. 4 . . 4 . . 4 . 4 . 4 . . 4 . 
-4 . . 4 . . 4 . . 4 . 4 . . 4 . 
-4 . 4 4 . 4 4 . 4 . . . 4 . . 4 
-4 . 4 . . 4 . . 4 . . . 4 . . 4 
-. . . 4 . . 4 . . 4 . . . 4 . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-b b b b b b b b b . . . . . . . 
-b b b f b c b b b . . . . . . . 
-b b b b b b c b b . . . . . . . 
-b f b b b b b c b . . . . . . . 
-b b b b b b b c b . . . . . . . 
-b c b b b b b b b b . . . . . . 
-b b c b b b b b b b b . . . . . 
-b b b c c b b b b . . . . . . . 
-b b b b b b b b b b . . . . . . 
-. . . . . b b . . b . . . . . . 
-. . . . . . b . . b b . . . . . 
-. . . . . . . . . . b b . . . . 
-. . . . . . . . . . . b b b b . 
-. . . . . . . . . . . . . . b b 
-. . . . . . . . . . . . . . . b 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. e . e . e . . e . . . . . . . 
-. e d d e d d e d . e . . . . . 
-. d d d d d d d d e d . e . . . 
-d d d d d d d d d d d e . . . d 
-d d d d d d d d d d d d . d d d 
-d f d f d d d d d d d d d d d . 
-d d d d d d d d d d d d d d d d 
-d d d d d d d d d d d d d d d . 
-d d d e e e e e e d d d . d d d 
-. d e e e e e e e e d . . . . d 
-. . e e e e e e e e . . . . . . 
-. . . d e e e d e d . . . . . . 
-. . d . . d . d . . d . . . . . 
-. . . . . . . . . . . . . . . . 
 `]
-game.onUpdateInterval(5000, function () {
-    projectile = sprites.createProjectileFromSide(animal_sprites[Math.randomRange(0, animal_sprites.length - 1)], -10, 0)
+animal_names = ["turtle", "crab", "green Fish", "octopus", "pink fish", "narwhal", "ray", "whale", "puffer fish", "shark"]
+game.onUpdateInterval(2000, function () {
+    animal_choice = Math.randomRange(0, animal_images.length - 1)
+    animal_speed = (animal_choice + 1) * -10
+    projectile = sprites.createProjectileFromSide(animal_images[animal_choice], animal_speed, 0)
     projectile.y = Math.randomRange(10, scene.screenHeight())
 })
