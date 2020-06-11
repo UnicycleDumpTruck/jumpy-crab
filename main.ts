dd52e089726199be3c3fd783db25f75edd875b6b
Jeff Highsmith <junk@jeffhighsmith.com>
2020-06-11 15:24:24 -0400

    Tested array of images to hold animals.

--- main.ts
+++ main.ts
@@ -39,8 +39,11 @@ namespace myTiles {
 `
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-	
+    music.baDing.play()
+    otherSprite.destroy()
+    info.changeScoreBy(1)
 })
+let projectile: Sprite = null
 scene.setBackgroundImage(img`
 a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a 
 a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a 
@@ -164,16 +167,16 @@ d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d d d d d d d 7 7 7 7 7 d d 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 `)
 let sub = sprites.create(img`
-. . . . . . . . . . . f f f f . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . f d e d e f . . . . . . . . . . . . . . . . 
-. . . . . . . . . . f d d e e f . . . . . . . . . . . . . . . . 
-. . . . . . . . . . f d e d e f . . . . . . . . . . . . . . . . 
-. . . . . . . . . . f d d e e f . . . . . . . . . . . . . . . . 
-. . f f f f f f f f d d e d e e f f f f f f f f f f f f f f . . 
-. f f b f d e d e e e e e e e e e e e e e e e e d e d f b f f . 
-f f b b f e d e e d d e e e e e e e e e e d d e e d e f b b f f 
-f b b b f d e e d b b d e e e e e e e e d b b d e e d f b b b f 
-f b b b f e e d b b b b d e e e e e e d b b b b d e e f b b b f 
+. . . . . . . . . . . f f f f f f . . . . . . . . . . . . . . . 
+. . . . . . . . . . f d e d e e e f . . . . . . . . . . . . . . 
+. . . . . . . . . . f d d e e e e f . . . . . . . . . . . . . . 
+. . . . . . . . . . f d e d e e e f . . . . . . . . . . . . . . 
+. . . . . . . . . . f d d e e e e f . . . . . . . . . . . . . . 
+. . f f f f f f f f d d e d e e e e f f f f f f f f f f f f . . 
+. f f c f d e d e e e e e e e e e e e e e e e e d e d f c f f . 
+f f c b f e d e e d d e e e e e e e e e e d d e e d e f b c f f 
+f c b b f d e e d c b d e e e e e e e e d c b d e e d f b b c f 
+f b b b f e e d c b b b d e e e e e e d c b b b d e e f b b b f 
 f f f f f e e d b b b b d e e e e e e d b b b b d e e f f f f f 
 f b b b f e e e d b b d e e e e e e e e d b b d e e e f b b b f 
 f b b b f e d e d d d e d e d e d e d e d d d e d e d f b b b f 
@@ -182,3 +185,41 @@ f f b b f d e d e d e d e d e d e d e d e d e d e d e f b b f f
 . . f f f f f f f f f f f f f f f f f f f f f f f f f f f f . . 
 `, SpriteKind.Player)
 controller.moveSprite(sub)
+let animal_sprites = [img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 f . . . . . 
+. . . . f 1 1 2 2 f . . . . . . 
+. . . f 1 1 1 1 1 f . . . . f f 
+. . f 1 1 1 1 2 1 1 1 f . f 2 2 
+. f 1 1 1 1 2 1 1 1 2 1 f 2 2 2 
+. f 1 f 1 f 1 2 1 2 1 1 2 2 2 f 
+f 1 1 1 1 1 2 1 1 1 2 1 2 2 2 f 
+1 1 1 1 1 1 1 2 1 2 1 1 2 2 2 f 
+f 1 1 1 1 1 2 1 1 1 2 1 f 2 2 2 
+. f 1 1 1 1 1 2 1 1 1 f . f 2 2 
+. . f 1 1 1 1 1 1 2 f . . . f f 
+. . . f 1 1 1 2 2 2 2 f . . . . 
+. . . . f f f f f 2 2 2 f . . . 
+. . . . . . . . . f f f f . . . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 f . . . . . 
+. . . . f 1 1 2 2 f . . . . . . 
+. . . f 1 1 1 1 1 f . . . . f f 
+. . f 1 1 1 1 2 1 1 1 f . f 2 2 
+. f 1 1 1 1 2 1 1 1 2 1 f 2 2 2 
+. f 1 f 1 f 1 2 1 2 1 1 2 2 2 f 
+f 1 1 1 1 1 2 1 1 1 2 1 2 2 2 f 
+1 1 1 1 1 1 1 2 1 2 1 1 2 2 2 f 
+f 1 1 1 1 1 2 1 1 1 2 1 f 2 2 2 
+. f 1 1 1 1 1 2 1 1 1 f . f 2 2 
+. . f 1 1 1 1 1 1 2 f . . . f f 
+. . . f 1 1 1 2 2 2 2 f . . . . 
+. . . . f f f f f 2 2 2 f . . . 
+. . . . . . . . . f f f f . . . 
+`]
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(animal_sprites[Math.randomRange(0, animal_sprites.length - 1)], 50, 100)
+})
